32b887f5f7fdfd6044aec3e8bb29df9d33429b65
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-06-05 16:14:26 +0600

    added verification email setup

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,8 +1,15 @@
 import { Message } from "@/model/Message";
+import { email } from "./../../node_modules/zod/dist/esm/v4/classic/schemas";
 
 export interface ApiResponse {
   success: boolean;
   message: string;
   isAcceptingMessages?: boolean;
-  messages: Array<Message>;
+  messages?: Array<Message>;
 }
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+) {}
